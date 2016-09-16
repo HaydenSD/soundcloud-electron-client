66563b1bde54d55ff7c7cddb3dda039a904ffbf6
HaydenSD <haydensd@protonmail.ch>
2016-09-16 11:27:03 -0400

    Added .eslintrc

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let win
 function createWindow () {
   win = new BrowserWindow({width: 1000, height: 800})
   win.loadURL('https://soundcloud.com')
+  win.webContents.openDevTools()
   win.on('closed', () => {
     win = nill
   } )
